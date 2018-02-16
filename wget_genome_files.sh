f53e221a86ac9df44bafbea1b4ec1a7157da631b
Jeff Jasper <jasper1918@gmail.com>
2018-02-16 14:06:03 -0500

    add HG002

--- wget_genome_files.sh
+++ wget_genome_files.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
+
+# Transcript GTF
 wget http://ftp.ensembl.org/pub/release-75/gtf/homo_sapiens/Homo_sapiens.GRCh37.75.gtf.gz
 gunzip Homo_sapiens.GRCh37.75.gtf.gz
+
+# Genome Fsta
 wget http://ftp.ensembl.org/pub/release-75/fasta/homo_sapiens/dna/Homo_sapiens.GRCh37.75.dna.primary_assembly.fa.gz
+
+# HG002-NA24385 Variants from NIST
+wget ftp://ftp-trace.ncbi.nlm.nih.gov/giab/ftp/release/AshkenazimTrio/HG002_NA24385_son/NISTv3.3.2/GRCh37/HG002_GRCh37_GIAB_highconf_CG-IllFB-IllGATKHC-Ion-10X-SOLID_CHROM1-22_v.3.3.2_highconf_triophased.vcf.gz
+wget ftp://ftp-trace.ncbi.nlm.nih.gov/giab/ftp/release/AshkenazimTrio/HG002_NA24385_son/NISTv3.3.2/GRCh37/HG002_GRCh37_GIAB_highconf_CG-IllFB-IllGATKHC-Ion-10X-SOLID_CHROM1-22_v.3.3.2_highconf_triophased.vcf.gz.tbi
